95767fef4c77c7f70af85cf48849c47674b0d680
Jeff Ferguson <chaosdesigns@shaw.ca>
2022-02-22 11:54:41 -0800

    Add About

--- Rick And Morty/CharacterListView.swift
+++ Rick And Morty/CharacterListView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 // _____________________________________________________________
 struct CharacterListView: View {
 	@StateObject private var model = CharacterListModel.shared // we just need one
+	@State private var showingAbout = false
 
 	var body: some View {
 		NavigationView {
@@ -21,6 +22,16 @@ struct CharacterListView: View {
 			.listStyle(PlainListStyle())	// and  GroupedListStyle and InsetListStyle  pushes the left and right boundaries out!
 			.listItemTint(Color .clear)
 			.navigationTitle("Characters")
+			.toolbar {
+				ToolbarItem(placement: .navigationBarTrailing) {
+					Button("About") {
+						showingAbout.toggle()
+					}
+				}
+			}
+			.sheet(isPresented: $showingAbout) {
+				AboutView()
+			}
 		}
 	}
 }
@@ -61,6 +72,48 @@ struct IconView : View {
 			.aspectRatio(contentMode: .fit)
 			.frame(width: size, height: size)
 			.cornerRadius(16)
+	}
+}
+
+// _____________________________________________________________
+struct AboutView: View {
+	@Environment(\.dismiss) var dismiss
+
+	var body: some View {
+		VStack {
+			Spacer()
+			Text("Rick And Morty App")
+				.font(.largeTitle)
+				.padding()
+
+			Text("Written by Jeff Ferguson")
+				.font(.body)
+				.padding()
+
+			Spacer()
+			Button(action: handleCloseButton ) {
+				CloseButtonView()
+			}
+			.padding()
+		}
+	}
+
+	// ____________
+	func handleCloseButton() {
+		dismiss()
+	}
+}
+
+// _____________________________________________________________
+struct CloseButtonView : View {
+	var body: some View {
+		return Text("Close")
+			.font(.headline)
+			.foregroundColor(.white)
+			.padding()
+			.frame(width: 150, height: 50)
+			.background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .top, endPoint: .bottom))
+			.cornerRadius(15.0)
 	}
 }
 
