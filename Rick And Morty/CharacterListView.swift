8faf5453d0639524ad29ebe372e891f8f4919976
Jeff Ferguson <chaosdesigns@shaw.ca>
2022-02-22 11:52:10 -0800

    initial CharacterListView

--- Rick And Morty/CharacterListView.swift
+++ Rick And Morty/CharacterListView.swift
@@ -7,12 +7,64 @@
 
 import SwiftUI
 
+// _____________________________________________________________
 struct CharacterListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@StateObject private var model = CharacterListModel.shared // we just need one
+
+	var body: some View {
+		NavigationView {
+			List {
+				ForEach(model.characters) { character in
+					CharacterListCell(character: character)
+				}
+			}
+			.listStyle(PlainListStyle())	// and  GroupedListStyle and InsetListStyle  pushes the left and right boundaries out!
+			.listItemTint(Color .clear)
+			.navigationTitle("Characters")
+		}
+	}
 }
 
+// _____________________________________________________________
+struct CharacterListCell: View {
+	var character: CharacterRec
+
+	var body: some View {
+		HStack {
+			IconView(image: character.avatar ?? UIImage(systemName: "person.circle")!  , size: 60.0)
+
+			VStack(alignment: .leading) {
+				Text("\(character.name)")
+					.font(.body)
+					.fontWeight(.bold)
+					.padding(.horizontal)
+
+				Text("Episodes: \(character.episodes)")
+					.font(.caption)
+					.foregroundColor(.secondary)
+					.padding(.horizontal)
+			}
+
+			Spacer()
+		}
+	}
+}
+
+//_________________________________________________________
+struct IconView : View {
+	var image: UIImage
+	var size: CGFloat
+
+	var body: some View {
+		return Image(uiImage: image)
+			.resizable()
+			.aspectRatio(contentMode: .fit)
+			.frame(width: size, height: size)
+			.cornerRadius(16)
+	}
+}
+
+// _____________________________________________________________
 struct CharacterListView_Previews: PreviewProvider {
     static var previews: some View {
         CharacterListView()
