c6858601c470b54518e2865f6ce472fac354ddfd
Jeff Ferguson <chaosdesigns@shaw.ca>
2022-02-22 12:17:01 -0800

    flesh out CharacterProfileView

--- Rick And Morty/CharacterProfileView.swift
+++ Rick And Morty/CharacterProfileView.swift
@@ -7,14 +7,48 @@
 
 import SwiftUI
 
+//_________________________________________________________
 struct CharacterProfileView: View {
 	var character: CharacterRec
 
 	var body: some View {
-        Text("Character Profile")
-    }
+		HStack {
+			Spacer()
+			VStack {
+
+				IconView(image: character.avatar ?? UIImage(systemName: "person.circle")!, size: 280.0)
+
+				Text("\(character.name)")
+					.font(.body)
+					.fontWeight(.bold)
+					.padding(.horizontal)
+					.padding(.bottom)
+
+				Text(character.status)
+					.font(.body)
+					.padding(.horizontal)
+
+				Text(character.species)
+					.font(.body)
+					.padding(.horizontal)
+
+				Text(character.gender)
+					.font(.body)
+					.padding(.horizontal)
+
+				Text(character.location)
+					.font(.body)
+					.padding(.horizontal)
+
+				Spacer()
+			}
+			Spacer()
+		}
+		.navigationTitle("Profile")
+	}
 }
 
+//_________________________________________________________
 struct CharacterProfileView_Previews: PreviewProvider {
     static var previews: some View {
         CharacterProfileView(character: testCharacters[0])
