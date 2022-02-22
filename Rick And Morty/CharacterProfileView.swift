d3179d01dff82259d0f4006f4a455f226931f3d7
Jeff Ferguson <chaosdesigns@shaw.ca>
2022-02-22 11:55:53 -0800

    initial commit for CharacterProfileView

---
+++ Rick And Morty/CharacterProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterProfileView.swift
+//  Rick And Morty
+//
+//  Created by Jeff Ferguson on 2022-02-22.
+//
+
+import SwiftUI
+
+struct CharacterProfileView: View {
+    var body: some View {
+        Text("Character Profile")
+    }
+}
+
+struct CharacterProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterProfileView()
+    }
+}
