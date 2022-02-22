8850683aa48960d71b8f3995c833e356d5726e92
Jeff Ferguson <chaosdesigns@shaw.ca>
2022-02-22 08:48:38 -0800

    Initial Commit

---
+++ Rick And Morty/Rick_And_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_And_MortyApp.swift
+//  Rick And Morty
+//
+//  Created by Jeff Ferguson on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct Rick_And_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
