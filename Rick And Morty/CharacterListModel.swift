18ebab49278c856c25b3181f9c7d104aa6727820
Jeff Ferguson <chaosdesigns@shaw.ca>
2022-02-22 08:56:13 -0800

    initial commit

---
+++ Rick And Morty/CharacterListModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterListModel.swift
+//  Rick And Morty
+//
+//  Created by Jeff Ferguson on 2022-02-22.
+//
+
+import Foundation
---
+++ Rick And Morty/CharacterListView.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterListView.swift
+//  Rick And Morty
+//
+//  Created by Jeff Ferguson on 2022-02-22.
+//
+
+import SwiftUI
+
+struct CharacterListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CharacterListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterListView()
+    }
+}
